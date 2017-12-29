bf7e0a6d4862d6d735c76dda1a444de6f54b7301
apkouk <cescorh@gmail.com>
2017-12-29 12:47:42 +0100

    MacroCommand added 2

---
+++ DesignPatterns/CommandPattern/MacroCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DesignPatterns.CommandPattern.Interfaces;
+
+namespace DesignPatterns.CommandPattern
+{
+    public class MacroCommand : Command
+    {
+        Command[] commands;
+
+        public MacroCommand(Command[] commands)
+        {
+            this.commands = commands;
+        }
+
+        public void execute()
+        {
+            for (int i = 0; i < commands.Length; i++)
+            {
+                commands[i].execute();
+            }
+        }
+
+        public void undo()
+        {
+            for (int i = commands.Length - 1; i >= 0; i--)
+            {
+                commands[i].undo();
+            }
+        }
+    }
+}
